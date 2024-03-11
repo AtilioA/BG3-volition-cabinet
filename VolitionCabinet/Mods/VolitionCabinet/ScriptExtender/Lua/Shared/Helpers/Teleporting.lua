751926f4456c1d5ad557bf5bb1fcec9998c507f9
Atílio <atiliodadalto@protonmail.com>
2024-03-11 20:49:06 -0300

    refactor: don't teleport linked characters on `Helpers.Teleporting:TeleportToPosition()`

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Teleporting.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Teleporting.lua
@@ -1,22 +1,21 @@
 ---@class HelperTeleporting: Helper
 Helpers.Teleporting = _Class:Create("HelperTeleporting", Helper)
 
--- TODO: create disjoint sets of linked characters in Helpers.Character
---- Teleports a character and linked characters to the specified position.
+--- Teleports a character and summons to the specified position.
 ---@param character string
 ---@param x number
 ---@param y number
 ---@param z number
 function Helpers.Teleporting:TeleportToPosition(character, x, y, z)
-  Osi.TeleportToPosition(character, x, y, z, "TeleportToPosition_" .. character, 1, 0, 1, 0, 1)
+  Osi.TeleportToPosition(character, x, y, z, "TeleportToPosition_" .. character, 0, 0, 1, 0, 1)
 end
 
---- Teleports the party members to the specified character's position.
+--- Teleports linked/grouped party members to the specified character's position.
 ---@param character string
-function Helpers.Teleporting:TeleportPartyMembersToCharacter(character)
+function Helpers.Teleporting:TeleportLinkedPartyMembersToCharacter(character)
   local x, y, z = Osi.GetPosition(character)
   if x and y and z then
-    local otherPartyMembers = Helpers.Character:GetOtherPartyMembers(character)
+    local otherPartyMembers = Helpers.Character:GetCharactersLinkedWith(character)
     for i, partyMember in ipairs(otherPartyMembers) do
       Helpers.Teleporting:TeleportToPosition(partyMember, x, y, z)
     end
