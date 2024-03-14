aba4acaed7302e17c4382f3c3c83d5d0f9489253
AtilioA <atiliogpg@protonmail.com>
2024-03-14 14:50:58 -0300

    feat: add `Book.lua` helper for detecting/retrieving books

---
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Book.lua
@@ -0,0 +1,35 @@
+--[[
+--- This file contains helper functions related to detecting/retrieving books in the game.
+--]]
+
+---@class HelperBook: Helper
+Helpers.Book = _Class:Create("HelperBook", Helper)
+
+--- Checks if an object is a Book.
+---@param object GUIDSTRING The object to check.
+---@return boolean - true if the object is a Book, false otherwise.
+function Helpers.Book:IsBook(object)
+  return Osi.GetBookID(object) ~= nil
+end
+
+--- Gets all Book items in a character's inventory.
+---@param character any character to check.
+---@param shallow boolean If true, recursively checks inside bags and containers.
+---@return table | nil - table of Book items in the character's inventory, or nil if none found.
+function Helpers.Book:GetBookInInventory(character, shallow)
+  local inventory = Helpers.Inventory:GetInventory(character, false, shallow)
+  local matchedItems = {}
+
+  for _, item in ipairs(inventory) do
+    local itemObject = Helpers.Object:GetItemUUID(item)
+    if self:IsBook(itemObject) then
+      table.insert(matchedItems, itemObject)
+    end
+  end
+
+  if #matchedItems > 0 then
+    return matchedItems
+  else
+    return nil
+  end
+end
