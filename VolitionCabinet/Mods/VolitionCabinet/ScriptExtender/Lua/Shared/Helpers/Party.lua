ee4d2930a69b7833be208ed2f12d981251540056
Atílio <atiliodadalto@protonmail.com>
2024-03-30 08:16:35 -0300

    feat: add new function to get all party members

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Party.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Party.lua
@@ -5,7 +5,7 @@
 ---@class HelperParty: Helper
 VCHelpers.Party = _Class:Create("HelperParty", Helper)
 
-
+-- Return the party members currently following the player
 function VCHelpers.Party:GetPartyMembers()
   local teamMembers = {}
 
@@ -15,6 +15,25 @@ function VCHelpers.Party:GetPartyMembers()
       teamMembers[#teamMembers + 1] = VCHelpers.Format:Guid(player[1])
     end
   end
+
+  return teamMembers
+end
+
+-- Return all party members, including the ones at camp
+-- NOTE: this also includes a mysterious Dragonborn_Blue_Male_Player character that is not really  part of the party?
+function VCHelpers.Party.GetAllPartyMembers()
+  local teamMembers = {}
+
+  local allPlayers = Osi.DB_PartOfTheTeam:Get(nil)
+  for _, player in ipairs(allPlayers) do
+    local isDummy = string.match(player[1]:lower(), "%f[%A]dummy%f[%A]")
+    local isJergal = string.match(player[1]:lower(), "%f[%A]jergalavatar%f[%A]")
+    if isDummy == nil and isJergal == nil then
+      teamMembers[#teamMembers + 1] = VCHelpers.Format:Guid(player[1])
+    end
+  end
+
+  _D(teamMembers)
 
   return teamMembers
 end
