039997b3d56ee45af968fae3ac269d503b233421
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-09 19:05:16 -0300

    feat: add `GetTranslatedStringFromTemplateUUID` method to `Loca` helper

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Loca.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Loca.lua
@@ -1,6 +1,16 @@
 ---@class HelperLoca: Helper
 VCHelpers.Loca = _Class:Create("HelperLoca", Helper)
 
+---@param object GUIDSTRING
+---@return string|nil
+function VCHelpers.Loca:GetTranslatedStringFromTemplateUUID(object)
+    local template = Ext.Template.GetTemplate(object)
+    if template and template.DisplayName and template.DisplayName.Handle then
+        return Ext.Loca.GetTranslatedString(template.DisplayName.Handle.Handle)
+    end
+    return nil
+end
+
 ---@param object any
 ---@return string|nil
 function VCHelpers.Loca:GetDisplayName(object)
@@ -13,7 +23,7 @@ function VCHelpers.Loca:GetDisplayName(object)
                 name = entity.DisplayName.Name
                 if (name == "" or name == nil) and self.IsServer then
                     name = Ext.Loca.GetTranslatedString(Ext.Template.GetTemplate(entity.OriginalTemplate
-                    .OriginalTemplate).DisplayName.Handle.Handle)
+                        .OriginalTemplate).DisplayName.Handle.Handle)
                 end
             end
         end
@@ -47,7 +57,7 @@ function VCHelpers.Loca:GetTitleName(object)
             name = Ext.Loca.GetTranslatedString(entity.DisplayName.UnknownKey.Handle.Handle)
             if name == "" or name == nil and self.IsServer then
                 name = Ext.Loca.GetTranslatedString(Ext.Template.GetTemplate(entity.OriginalTemplate.OriginalTemplate)
-                .Title.Handle.Handle)
+                    .Title.Handle.Handle)
             end
         end
     end
