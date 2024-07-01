05a456729be31e8291f23be5aaa1751634ef16e5
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-07-01 16:31:39 -0300

    refactor: improve teleporting functions abstraction

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Teleporting.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Teleporting.lua
@@ -12,16 +12,28 @@ function VCHelpers.Teleporting:TeleportToPosition(character, x, y, z)
 end
 
 --- Teleports linked/grouped party members to the specified character's position.
---- This is better than teleporting the character to itself with teleportLinkedCharacters set to true because it will not interrupt the character's current action.
+--- This is better than teleporting the character to itself with teleportLinkedCharacters set to true since it will not interrupt the character's current action.
 ---@param character string
 function VCHelpers.Teleporting:TeleportLinkedPartyMembersToCharacter(character)
-  local x, y, z = Osi.GetPosition(character)
+  local otherPartyMembers = VCHelpers.Character:GetCharactersLinkedWith(character)
+  if #otherPartyMembers > 0 then
+    VCHelpers.Teleporting:TeleportCharactersToCharacter(character, otherPartyMembers)
+  else
+    VCWarn(0, "VCHelpers.Teleporting:TeleportLinkedPartyMembersToCharacter() - No linked party members found.")
+  end
+end
+
+--- Teleports a table of characters to the specified character's position.
+--- This is useful for moving a group of characters to a leader's position.
+---@param targetCharacter string
+---@param charactersToTeleport table
+function VCHelpers.Teleporting:TeleportCharactersToCharacter(targetCharacter, charactersToTeleport)
+  local x, y, z = Osi.GetPosition(targetCharacter)
   if x and y and z then
-    local otherPartyMembers = VCHelpers.Character:GetCharactersLinkedWith(character)
-    for i, partyMember in ipairs(otherPartyMembers) do
-      VCHelpers.Teleporting:TeleportToPosition(partyMember, x, y, z)
+    for _, character in ipairs(charactersToTeleport) do
+      VCHelpers.Teleporting:TeleportToPosition(character, x, y, z)
     end
   else
-    VCWarn(0, "VCHelpers.Teleporting:TeleportPartyMembersToCharacter() - Character position not found.")
+    VCWarn(0, "VCHelpers.Teleporting:TeleportCharactersToCharacter() - Target character position not found.")
   end
 end
