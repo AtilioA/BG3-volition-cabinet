9d259c208fd76d58431bfc7de42fa4b14250e795
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-07-09 20:43:27 -0300

    refactor: remove unused event `ShortRested` from 'custom' events

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Classes/Events/SubscribableEvents/Osiris/_Init.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Classes/Events/SubscribableEvents/Osiris/_Init.lua
@@ -7,7 +7,6 @@ RequireFiles("Shared/Classes/Events/SubscribableEvents/Osiris/", {
     "ObjectTimerFinished",
     "RequestCanCombine",
     "Saw",
-    "ShortRested",
     "StartedPreviewingSpell",
     "StatusApplied",
     "StatusRemoved",
