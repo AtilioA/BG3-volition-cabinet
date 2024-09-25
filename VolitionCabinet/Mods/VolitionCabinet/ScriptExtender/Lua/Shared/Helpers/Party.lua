f5824bb0d4f47e18d8304ab3e216cca0937d062f
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-09-25 12:58:00 -0300

    refactor: add nil checks to `VCHelpers.Party:GetAllPartyMembers()`

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Party.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Party.lua
@@ -7,34 +7,39 @@ VCHelpers.Party = _Class:Create("HelperParty", Helper)
 
 -- Return the party members currently following the player
 function VCHelpers.Party:GetPartyMembers()
-  local teamMembers = {}
+    local teamMembers = {}
 
-  local allPlayers = Osi.DB_Players:Get(nil)
-  for _, player in ipairs(allPlayers) do
-    if not string.match(player[1]:lower(), "%f[%A]dummy%f[%A]") then
-      teamMembers[#teamMembers + 1] = VCHelpers.Format:Guid(player[1])
+    local allPlayers = Osi.DB_Players:Get(nil)
+    for _, player in ipairs(allPlayers) do
+        if not string.match(player[1]:lower(), "%f[%A]dummy%f[%A]") then
+            teamMembers[#teamMembers + 1] = VCHelpers.Format:Guid(player[1])
+        end
     end
-  end
 
-  return teamMembers
+    return teamMembers
 end
 
 -- Return all party members, including the ones at camp
-function VCHelpers.Party.GetAllPartyMembers()
-  local teamMembers = {}
+---@return Guid[] teamMembers A table of guids of all party members
+function VCHelpers.Party:GetAllPartyMembers()
+    local teamMembers = {}
 
-  local allPlayers = Osi.DB_PartOfTheTeam:Get(nil)
-  for _, player in ipairs(allPlayers) do
-    local isDummy = string.match(player[1]:lower(), "%f[%A]dummy%f[%A]")
-    local isJergal = string.match(player[1]:lower(), "%f[%A]jergalavatar%f[%A]")
-    if isDummy == nil and isJergal == nil then
-      teamMembers[#teamMembers + 1] = VCHelpers.Format:Guid(player[1])
+    local teamplayer = Osi.DB_PartOfTheTeam:Get(nil)
+    if not teamplayer then return teamMembers end
+
+    for _, player in ipairs(teamplayer) do
+        if player and player[1] then
+            local playerLower = player[1]:lower()
+            local isDummy = string.match(playerLower, "%f[%A]dummy%f[%A]")
+            local isJergal = string.match(playerLower, "%f[%A]jergalavatar%f[%A]")
+            if isDummy == nil and isJergal == nil then
+                teamMembers[#teamMembers + 1] = VCHelpers.Format:Guid(player[1])
+            end
+        end
     end
-  end
 
-  return teamMembers
+    return teamMembers
 end
-
 
 -- Function to return all other party members
 ---@param characterGuid Guid
@@ -43,11 +48,11 @@ function VCHelpers.Party:GetOtherPartyMembers(characterGuid)
     local otherPartyMembers = {}
     local companions = Osi.DB_Players:Get(nil)
     for i, companion in ipairs(companions) do
-      local companionGuid = VCHelpers.Format:Guid(tostring(companion[1]))
-      if companionGuid ~= characterGuid then
-        table.insert(otherPartyMembers, companionGuid)
-      end
+        local companionGuid = VCHelpers.Format:Guid(tostring(companion[1]))
+        if companionGuid ~= characterGuid then
+            table.insert(otherPartyMembers, companionGuid)
+        end
     end
 
     return otherPartyMembers
-  end
+end
