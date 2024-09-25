10de34d2a830f8946df38fece00a3de611f948b6
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-09-25 12:58:33 -0300

    fix: account for party followers in camp for `VCHelpers.Dialog:CheckIfPartyInvolved()`

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Dialog.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Dialog.lua
@@ -1,7 +1,10 @@
 ---@class HelperDialog: Helper
 VCHelpers.Dialog = _Class:Create("HelperDialog", Helper)
 
+
 -- Returns a table with all the NPCs involved in an automated dialog instance
+---@param instanceID integer The ID of the dialog instance
+---@return GUIDSTRING[] - involvedNPCs A table of guids of NPCs involved in the dialog
 function VCHelpers.Dialog:GetInvolvedNPCs(instanceID)
     local involvedNPCs = {}
 
@@ -16,12 +19,12 @@ function VCHelpers.Dialog:GetInvolvedNPCs(instanceID)
     return involvedNPCs
 end
 
+-- Checks if the list of NPCs contains any of the player's party members
+---@param involvedNPCs GUIDSTRING[] A table of guids of NPCs involved in the dialog
+---@return boolean - True if any of the NPCs are part of the player's companions (includes ones at camp)
 function VCHelpers.Dialog:CheckIfPartyInvolved(involvedNPCs)
-    for _, npc in ipairs(involvedNPCs) do
-        if Osi.IsInPartyWith(npc, Osi.GetHostCharacter()) == 1 then
-            return true
-        end
-    end
+    local partyMembers = VCHelpers.Party:GetAllPartyMembers()
+    if not partyMembers then return false end
 
-    return false
+    return table.hasCommonElements(involvedNPCs, partyMembers)
 end
