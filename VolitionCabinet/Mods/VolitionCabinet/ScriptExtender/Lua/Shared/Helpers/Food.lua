35c80b50d29c66daa7fbfef5d471622fca7e4d8b
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-05-13 20:31:09 -0300

    refactor: enhance food helper functions with new checks for consumable and alcoholic items

--- VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Food.lua
+++ VolitionCabinet/Mods/VolitionCabinet/ScriptExtender/Lua/Shared/Helpers/Food.lua
@@ -9,16 +9,28 @@ VCHelpers.Food = _Class:Create("HelperFood", Helper)
 ---@param object GUIDSTRING
 ---@return boolean
 function VCHelpers.Food:IsFood(object)
-  if VCHelpers.Object:IsItem(object) then
-    return Osi.ItemGetSupplyValue(object) > 0
-  end
+    if VCHelpers.Object:IsItem(object) then
+        return Osi.ItemGetSupplyValue(object) > 0
+    end
 end
 
 --- Check if an object is a beverage item.
 ---@param object GUIDSTRING
 ---@return boolean
-function VCHelpers.Food:IsBeverage(object)
-  return self:IsFood(object) and Osi.IsConsumable(object) == 1
+function VCHelpers.Food:IsConsumableFood(object)
+    return self:IsFood(object) and Osi.IsConsumable(object) == 1
+end
+
+function VCHelpers.Food:IsAlcoholicItem(object)
+    if not object then return false end
+    if not self:IsConsumableFood(object) then return false end
+    local itemEntity = Ext.Entity.Get(object)
+    if not itemEntity then return false end
+    if not itemEntity.UseAction then return false end
+    if not itemEntity.UseAction.UseActions then return false end
+    if not itemEntity.UseAction.UseActions[1] then return false end
+    if not itemEntity.UseAction.UseActions[1]   .StatsId then return false end
+    return itemEntity.UseAction.UseActions[1].StatsId == "DRINK_ALCOHOL"
 end
 
 --- Gets all food items in a character's inventory.
@@ -26,19 +38,19 @@ end
 ---@param shallow boolean If true, recursively checks inside bags and containers.
 ---@return table | nil - table of food items in the character's inventory, or nil if none found.
 function VCHelpers.Food:GetFoodInInventory(character, shallow)
-  local inventory = VCHelpers.Inventory:GetInventory(character, false, shallow)
-  local matchedItems = {}
+    local inventory = VCHelpers.Inventory:GetInventory(character, false, shallow)
+    local matchedItems = {}
 
-  for _, item in ipairs(inventory) do
-    local itemObject = item.TemplateName .. item.Guid
-    if VCHelpers.Food:IsFood(itemObject) then
-      table.insert(matchedItems, itemObject)
+    for _, item in ipairs(inventory) do
+        local itemObject = item.TemplateName .. item.Guid
+        if VCHelpers.Food:IsFood(itemObject) then
+            table.insert(matchedItems, itemObject)
+        end
     end
-  end
 
-  if #matchedItems > 0 then
-    return matchedItems
-  else
-    return nil
-  end
+    if #matchedItems > 0 then
+        return matchedItems
+    else
+        return nil
+    end
 end
